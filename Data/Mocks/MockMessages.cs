7de0e5b1301892852673417ea0f75188f2c04ea0
Anna Andreikovets <ann.andreikovets@gmail.com>
2023-07-13 19:00:08 +0300

    Add view for mail

--- Data/Mocks/MockMessages.cs
+++ Data/Mocks/MockMessages.cs
@@ -4,7 +4,15 @@ namespace DatingSite.Data.Mocks
 {
     public class MockMessages
     {
-        static List<Chat> chats = new List<Chat>();
+        static List<Chat> chats = new List<Chat>()
+        {
+            new Chat()
+            {
+                Id = Guid.NewGuid(),
+                PersonId = Guid.NewGuid(), //число для теста
+                Name = "Some name"
+            }
+        };
         //сюда добавить чат с каким-нибудь человечком
         static public List<Chat> Chats { get { return chats; } set { chats.AddRange(value); } }
     }
--- Data/Models/Chat.cs
+++ Data/Models/Chat.cs
@@ -4,6 +4,9 @@ namespace DatingSite.Data.Models
     {
         public Guid Id { get; set; }
         public Guid PersonId { get; set; }
+        public string Name { get; set;} = null!;
+        public short UreadMessagesCount { get; set; }
+        public string LastMessage { get; set; } = "The user reciprocated! Start a chat.";
         public List<Message>? Messages { get; set; }
     }
 }
